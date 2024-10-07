31382892b07fa7d52bf69cc375cc246924b0d066
Berikisu Ibnrahim <berikisuibrahim227@gmail.com>
2024-10-07 16:11:19 +0000

    made changes

--- models/review.js
+++ models/review.js
@@ -4,6 +4,8 @@ import { Schema, model, Types } from "mongoose";
 const reviewSchema = new Schema({
     rating:{type: Number},
     comment:{type: String},
-    book:{type: Types.ObjectId, ref: 'Book'},
+    book:{type: Types.ObjectId, ref: 'Review'},
     // user:{type: Types.ObjectId, ref: 'User'}
 });
+
+export const Review = model('Review', reviewSchema)
