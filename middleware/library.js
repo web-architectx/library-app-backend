752d0f76ba2328795bda8f296b4276e920222672
roblack66 <romeoasante66@gmail.com>
2024-10-09 09:18:06 +0000

    library joi added

--- middleware/library.js
+++ middleware/library.js
@@ -1,8 +1,18 @@
-import Joi, { string } from "joi";
+import Joi from "joi";
 
-const validationSchema = Joi.object({
+const libraryValidation = Joi.object({
 title: Joi.string().required(),
 author: Joi.string(),
 publishedYear:Joi.number(),
 genre: Joi.string().optional()
 });
+
+export const  libraryValidate = (req,res,next) => {
+    try {
+        const {error} = libraryValidation.validate(req.body)
+        res.status(400).json(error)
+    } catch (error) {
+        next(error)     
+    }
+}
+
--- routes/library.js
+++ routes/library.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { addBooks, deleteBooks, getAllBooks, getBookById, updateBooks } from "../controllers/library.js";
+import { libraryValidate } from "../middleware/library.js";
+
 
 export const libraryRouter = Router()
 
-libraryRouter.post('/library', addBooks)
+libraryRouter.post('/library', libraryValidate, addBooks)
 libraryRouter.get('/library', getAllBooks)
 libraryRouter.get('/library/:id',getBookById )
-libraryRouter.patch('/library/:id',updateBooks)
+libraryRouter.patch('/library/:id',libraryValidate, updateBooks)
 libraryRouter.delete('/library/:id', deleteBooks)
 
+export default libraryRouter
